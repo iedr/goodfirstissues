a7315556424b6037e1cfff2448538f419aa7c48e
Julieta <juliiy98@gmail.com>
2020-12-15 16:48:58 -0700

    fixed identations

--- scripts/issue.js
+++ scripts/issue.js
@@ -33,81 +33,80 @@
     }
 */
 class Issue {
-  constructor(issue) {
-    this.num_assignees = issue.Issue.Assignees.issue_assignees_count;
-    this.num_comments = issue.Issue.Comments.issue_comment_count;
-    this.created_at = issue.Issue.issue_createdAt;
-    this.issue_labels_count = issue.Issue.issue_labels.label_totalcount;
-    this.issue_repo_desc = issue.Issue.issue_repo.repo_desc;
-    this.issue_repo_name = issue.Issue.issue_repo.repo_name;
-    this.issue_repo_url = issue.Issue.issue_repo.repo_url;
-    this.issue_repo_stars = issue.Issue.issue_repo.repo_stars;
-    this.issue_title = issue.Issue.issue_title;
-    this.issue_url = issue.Issue.issue_url;
-    this.owner_login = issue.Issue.issue_repo.Owner.repo_owner;
+    constructor(issue) {
+        this.num_assignees = issue.Issue.Assignees.issue_assignees_count;
+        this.num_comments = issue.Issue.Comments.issue_comment_count;
+        this.created_at = issue.Issue.issue_createdAt;
+        this.issue_labels_count = issue.Issue.issue_labels.label_totalcount;
+        this.issue_repo_desc = issue.Issue.issue_repo.repo_desc;
+        this.issue_repo_name = issue.Issue.issue_repo.repo_name;
+        this.issue_repo_stars = issue.Issue.issue_repo.repo_stars;
+        this.issue_repo_url = issue.Issue.issue_repo.repo_url;
+        this.issue_title = issue.Issue.issue_title;
+        this.issue_url = issue.Issue.issue_url;
+        this.owner_login = issue.Issue.issue_repo.Owner.repo_owner;
 
-    let issue_labels = [];
-    for (let n of issue.Issue.issue_labels.Nodes) {
-      issue_labels.push(n.label_name);
+        let issue_labels = [];
+        for (let n of issue.Issue.issue_labels.Nodes) {
+            issue_labels.push(n.label_name);
+        }
+        this.issue_labels = issue_labels;
+
+        let repo_prog_lang = [];
+        for (let n of issue.Issue.issue_repo.repo_langs.Nodes) {
+            repo_prog_lang.push(n.repo_prog_language);
+        }
+        this.issue_repo_langs = repo_prog_lang;
     }
-    this.issue_labels = issue_labels;
 
-    let repo_prog_lang = [];
-    for (let n of issue.Issue.issue_repo.repo_langs.Nodes) {
-      repo_prog_lang.push(n.repo_prog_language);
+    getNumAssignees() {
+        return this.num_assignees;
     }
-    this.issue_repo_langs = repo_prog_lang;
-  }
 
-  getNumAssignees() {
-    return this.num_assignees;
-  }
+    getNumComments() {
+        return this.num_comments;
+    }
 
-  getNumComments() {
-    return this.num_comments;
-  }
+    getCreatedAt() {
+        return this.created_at;
+    }
 
-  getCreatedAt() {
-    return this.created_at;
-  }
+    getIssueLabelsCount() {
+        return this.issue_labels_count;
+    }
 
-  getIssueLabelsCount() {
-    return this.issue_labels_count;
-  }
+    getIssueRepoDesc() {
+        return this.issue_repo_desc;
+    }
 
-  getIssueRepoDesc() {
-    return this.issue_repo_desc;
-  }
+    getIssueRepoName() {
+        return this.issue_repo_name;
+    }
 
-  getIssueRepoName() {
-    return this.issue_repo_name;
-  }
+    getIssueRepoUrl() {
+        return this.issue_repo_url;
+    }
 
-  getIssueRepoUrl() {
-    return this.issue_repo_url;
-  }
+    getIssueTitle() {
+        return this.issue_title;
+    }
 
-  getIssueTitle() {
-    return this.issue_title;
-  }
+    getIssueUrl() {
+        return this.issue_url;
+    }
 
-  getIssueUrl() {
-    return this.issue_url;
-  }
+    getIssueLabels() {
+        return this.issue_labels;
+    }
 
-  getIssueLabels() {
-    return this.issue_labels;
-  }
+    getRepoProgLangs() {
+        return this.issue_repo_langs;
+    }
 
-  getRepoProgLangs() {
-    return this.issue_repo_langs;
-  }
-
-  getOwnerLogin() {
-    return this.owner_login;
-  }
-
-  getIssueRepoStars() {
-    return this.issue_repo_stars;
-  }
+    getOwnerLogin() {
+        return this.owner_login;
+    }
+    getIssueRepoStars() {
+        return this.issue_repo_stars;
+      }
 }
